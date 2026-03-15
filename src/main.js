82af8c6766ac567d9daa65aae7a46089281f5f98
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-03-14 21:45:31 -0700

    Added the sevenpens icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import faviconUrl from './assets/7P Logo Core.svg?url'
 
 const app = mount(App, {
   target: document.getElementById('app'),
 })
+
+const link = document.querySelector('link[rel="icon"]')
+if (link) link.href = faviconUrl
 
 export default app
